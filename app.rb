fd8b2a38839644c8e9a35000a3a936510e2af5cb
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-27 17:46:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+  # testing
+  get '/' do 
+    "testing"
+  end
 
   # This is a sample static route.
   get '/hello' do
@@ -13,5 +18,13 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/goodbye/:name' do 
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+  end
 
+  get '/multiply/:num1/:num2' do 
+    @product = params[:num1].to_i * params[:num2].to_i
+    status 200
+  end
 end
